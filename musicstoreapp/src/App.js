ac7c27025ea38727ebcda89f0a84929c15a5c83f
Shahrukh Khan <shahrukh.khan@bglgroup.co.uk>
2020-06-06 16:03:10 +0100

    Added renderAlbums function to bring bag albums of desired artist

--- musicstoreapp/src/App.js
+++ musicstoreapp/src/App.js
@@ -1,57 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import * as actions from './components/AlbumList';
 
-import AlbumList from './AlbumList'
+class Home extends Component {
+  state = {
+    albums:[]
+  } 
 
-function App() {
-  return (
-    <div className="App">
-      <div className="navbar">
-        <ul className="navItems">
-          <li>Home</li>
-          <li>MusicStoreLogo</li>
-          <li>Basket</li>
-        </ul>
-      </div>
+  componentDidMount(){
+    actions.getAlbums().then(item => this.setState({
+      albums  : item
+    }));
+  }
+  renderAlbums = () => {
+      const {albums} = this.state;
+      return albums && albums.length ?
+        albums.map((item,index) => (
+          <div key={index} className="col-md-4 mb-2">
+            <div className="card border-danger">
+              <img src={item.album.cover_big} alt="" className="card-img-top"/>
+              <div className="card-body">
+                <span className="text-primary">Eminem</span>
+                <div className="card-title">
+                  album title
+                </div>
+              </div>
+              <div className="card-footer">
+                <div className="links">
+                  <a href="#"><i className="fas fa-info text-danger"></i></a>
+                  <a href="#"><i className="fas fa-star text-danger"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))
+      : null;
+  }
 
-      <AlbumList />
-
-      <div className="main">
-        <div className="newReleases">
-          <a>New Releases</a>
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <div className="navbar">
+          <ul className="navItems">
+            <li>Home</li>
+            <li>MusicStoreLogo</li>
+            <li>Basket</li>
+          </ul>
         </div>
-
-        <div className="categories">
-          <div className="section">
-            <a>Vinyl</a>
-          </div>
-          <div className="section">
-            <a>CD</a>
-          </div>
-          <div className="section">
-            <a>My Collection</a>
-          </div>
-          <div className="section">
-            <a>Wishlist</a>
-          </div>
-        </div>
-
-        <div className="trendingReleases">
-          <h4 className="trendingTitle">Trending Releases</h4>
-          <div class="gallery js-flickity"
-            data-flickity-options='{ "wrapAround": true }'>
-            <div class="gallery-cell"></div>
-            <div class="gallery-cell"></div>
-            <div class="gallery-cell"></div>
-            <div class="gallery-cell"></div>
-            <div class="gallery-cell"></div>
-          </div>
-        </div>
+  
         
+  
+        <div className="main">
+          <div className="newReleases">
+            {this.renderAlbums()}
+          </div>
+  
+          <div className="categories">
+            <div className="section">
+              <a>Vinyl</a>
+            </div>
+            <div className="section">
+              <a>CD</a>
+            </div>
+            <div className="section">
+              <a>My Collection</a>
+            </div>
+            <div className="section">
+              <a>Wishlist</a>
+            </div>
+          </div>
+  
+          <div className="trendingReleases">
+            <h4 className="trendingTitle">Trending Releases</h4>
+            <div class="gallery js-flickity"
+              data-flickity-options='{ "wrapAround": true }'>
+              <div class="gallery-cell"></div>
+              <div class="gallery-cell"></div>
+              <div class="gallery-cell"></div>
+              <div class="gallery-cell"></div>
+              <div class="gallery-cell"></div>
+            </div>
+          </div>
+          
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+
+export default Home;
---
+++ musicstoreapp/src/components/AlbumList.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '06254c8926msh3b4318691ce804dp180048jsn3bdc56645c6c';
+
+const request = axios.create({
+    baseURL : 'https://deezerdevs-deezer.p.rapidapi.com/',
+    timeout : 3000,
+    headers  : {'X-RapidApi-Key': API_KEY}
+});
+
+export function getAlbums(search = 'eminem'){
+    const albums = request.get(`search?q=${search}`)
+        .then(respone => respone.data.data)
+        .catch(error => console.log(error));
+    return albums
+} 
